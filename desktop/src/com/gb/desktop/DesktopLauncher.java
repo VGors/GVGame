0c4c18e69c0d5600d57fad30d0af949fab512f4c
Victor <v.gorshenev@gmail.com>
2021-05-27 11:09:37 +0500

    Initial commit

---
+++ desktop/src/com/gb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gb.GVGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GVGame(), config);
+	}
+}
